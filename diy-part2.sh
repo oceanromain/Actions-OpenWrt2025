cab17f03aeaa0e9bd37f6e39864f8c3b9a6438db
oceanromain <68986251+oceanromain@users.noreply.github.com>
2025-12-16 07:36:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -87,7 +87,7 @@ git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/l
 #20241025
 #sed -i '8i \ \ \ \ \ \ \ \ entry({"admin", "control"}, firstchild(), "控制", 44).dependent = false' ./feeds/oceanweb/luci-app-autotimeset/luasrc/controller/autotimeset.lua 
 #sed -i '9d' ./feeds/oceanweb/luci-app-autotimeset/luasrc/controller/autotimeset.lua 
-20251216
+#20251216
 #sed -i 's/高级重启/关机/g' feeds/luci/applications/luci-app-advanced-reboot/po/zh-cn/advanced-reboot.po
 #chmod +x feeds/kenzo/luci-app-lucky/luci-app-lucky/root/etc/init.d/lucky 
 #chmod +x feeds/oceanweb/luci-app-autotimeset/root/etc/init.d/autotimeset
