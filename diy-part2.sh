1fc8f02c7b41d040a55ac52271fb69ffc1451482
oceanromain <68986251+oceanromain@users.noreply.github.com>
2026-05-09 06:10:34 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -34,8 +34,9 @@ echo "--------------------------------------------------------------------------
 #删除watchcat
 rm -rf feeds/packages/lang/golang
 rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
-rm -rf feeds/packages/utils/v2dat
+rm -rf rm -rf feeds/packages/net/{alist,adguardhome,mosdns,smartdns}
+#rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+#rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/kenzo/mihomo
 
 
